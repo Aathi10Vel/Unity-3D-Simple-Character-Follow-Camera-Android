0ec0ef4694ed63b9f8f3e75d589a89e8d2afa180
Aathi <37983032+Aathi10Vel@users.noreply.github.com>
2018-04-16 17:59:18 +0530

    Add files via upload

---
+++ FollowCamera.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class FollowCamera : MonoBehaviour {
+
+	public Transform targetPlayer;
+	Camera playerCamera;
+	public PlayerMove getActions; // this will get the actions from the PLayermoveScript
+	public float cameraDistance=5.0f;
+	public float cameraSensitivity = 3.0f;
+	public float camSmooth=2.0f;
+
+	public float charSmooth=5.0f;
+	public float smoothVel=10.0f;
+
+	public Vector2 mouseLook;
+	public Vector2 rotateVert;
+
+	public Quaternion camRotateX;
+	public Quaternion camRotateXY;
+
+	public Vector3 lookOffset;
+	public Vector3 camPosition;
+
+	public Button switchButton;
+	public Text dispText;
+	public bool camSwitch;
+
+
+	// Use this for initialization
+	void Start () {
+		camSwitch = true;
+		getActions = FindObjectOfType<PlayerMove> ();
+		playerCamera = GetComponent<Camera> ();
+		lookOffset = playerCamera.transform.position - targetPlayer.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (camSwitch == true) {
+			dispText.text = "TPS";
+		}
+		camControl ();	
+	}
+
+	public void camControl(){
+		var mouseDir = new Vector2 (CrossPlatformInputManager.GetAxisRaw ("Mouse X"), CrossPlatformInputManager.GetAxisRaw ("Mouse Y"));
+		mouseDir = Vector2.Scale (mouseDir, new Vector2 (cameraSensitivity* camSmooth, cameraSensitivity* camSmooth));
+		rotateVert.x = Mathf.Lerp (rotateVert.x, mouseDir.x, 1f / camSmooth);
+		rotateVert.y = Mathf.Lerp (rotateVert.y, mouseDir.y, 1f / camSmooth);
+		mouseLook += rotateVert;
+		mouseLook.y = Mathf.Clamp (mouseLook.y, -40, 40);
+
+		camRotateX = Quaternion.Euler (0, mouseLook.x, 0);
+		camRotateXY = Quaternion.Euler (-mouseLook.y, mouseLook.x, 0);
+
+		if (getActions.actionPerform == true) {
+			// This can smooth the Player Turn
+			targetPlayer.eulerAngles = Vector3.up * Mathf.SmoothDampAngle (targetPlayer.transform.eulerAngles.y, camRotateX.eulerAngles.y, ref smoothVel, charSmooth * Time.deltaTime);
+		} else {
+			Vector3 lookPoint = targetPlayer.transform.position;
+			playerCamera.transform.LookAt (lookPoint + lookOffset);
+		}
+
+		switchButton.onClick.AddListener (switchingCamera);
+
+		switch (camSwitch) {
+		case true:
+			{
+				camPosition=targetPlayer.position-(camRotateX*new Vector3(0,0,0)+new Vector3(0,-lookOffset.y,-lookOffset.z));
+				break;
+			}
+		case false:
+			{
+				camPosition=targetPlayer.position-(camRotateXY*Vector3.forward*cameraDistance+new Vector3(0,-lookOffset.y,0));
+				break;
+			}
+		default:
+			{
+				Debug.Log ("Not Working");
+				break;
+			}
+		}
+
+		playerCamera.transform.rotation = camRotateXY;
+		playerCamera.transform.position = camPosition;
+
+	}
+
+	//This Method Can Call for Switching Camera by the UI Button inside the CamControl
+	public void switchingCamera(){
+		if (camSwitch == true) {
+			camSwitch = false;
+			dispText.text = "FPS";
+		} else {
+			camSwitch = true;
+			dispText.text = "TPS";
+		}
+	}
+}
---
+++ PlayerMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput; // for Using the Default Joystick from the Standard Asset.
+
+public class PlayerMove : MonoBehaviour {
+
+	public CharacterController playerController;
+	public float playerSpeed=10.0f;
+	public float playerGravity=20.0f;
+	public float vGravity;
+	public bool actionPerform;
+	public Vector3 moveDirections;
+
+	// Use this for initialization
+	void Start () {
+		actionPerform = false;
+		playerController=GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		playerMovement ();
+	}
+
+	public void playerMovement(){
+		if (moveDirections.x == 0 && moveDirections.z == 0) {
+			actionPerform = false;
+		} else {
+			actionPerform = true;
+		}
+
+		moveDirections = new Vector3 (CrossPlatformInputManager.GetAxis ("Horizontal"), 0, CrossPlatformInputManager.GetAxis ("Vertical"));
+		moveDirections = transform.TransformDirection (moveDirections);
+		vGravity -= playerGravity * Time.deltaTime;
+		moveDirections *= playerSpeed;
+		moveDirections.y = vGravity;
+		playerController.Move (moveDirections * Time.deltaTime);
+	}
+}
